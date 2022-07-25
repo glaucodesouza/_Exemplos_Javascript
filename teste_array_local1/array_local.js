e2896b9fb7f1c81ec1375c935655c8c3e22dcf28
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2022-07-24 22:15:00 -0300

    24/07/2022 v1

---
+++ teste_array_local1/array_local.js
@@ -0,0 +1,14 @@
+function testeArray(p_array){
+    
+debugger;
+if (1==2) {
+    let arrayLocalFuncao = [];
+};
+return arrayLocalFuncao;
+};
+
+debugger;
+let meuArrayGlobal = [1, 2, 3, 4];
+console.log(meuArrayGlobal);
+testeArray(meuArrayGlobal);
+debugger;
---
+++ teste_array_local2/array_local.js
@@ -0,0 +1,14 @@
+function testeArray(){
+    
+debugger;
+    let arrayLocalFuncao = [];
+    arrayLocalFuncao[0] = 1;
+return arrayLocalFuncao;
+};
+
+debugger;
+
+let array1 = testeArray();
+console.log(array1);
+
+debugger;
