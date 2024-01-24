a7195ca80b77c38ba046369885d4714a8f658496
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-24 12:06:19 -0300

    Multi Select component

---
+++ ComboboxMultipleValues-js/script.js
@@ -0,0 +1,16 @@
+// Return an array of the selected opion values
+// select is an HTML select element
+function getSelectValues(select) {
+  var result = [];
+  var options = select && select.options;
+  var opt;
+
+  for (var i = 0, iLen = options.length; i < iLen; i++) {
+    opt = options[i];
+
+    if (opt.selected) {
+      result.push(opt.value || opt.text);
+    }
+  }
+  return result;
+}
