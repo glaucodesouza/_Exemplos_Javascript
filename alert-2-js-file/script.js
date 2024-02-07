b4c24c67e7c9ade1e2dbb55141b9bfef85a21750
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-07 13:00:59 -0300

    alert

---
+++ alert-2-js-file/script.js
@@ -0,0 +1,12 @@
+// debugger;
+let js = "amazing";
+if (js === "amazing") {
+  alert("Javascript is amazing");
+  console.log("Javascript is awesome");
+} else {
+  alert("Javascript is not amazing");
+  console.log("Javascript is not amazing");
+}
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
