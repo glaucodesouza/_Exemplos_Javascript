23a2a002e599699083f80bb296d75ddbdace51c9
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-07 22:32:45 -0300

    variables

---
+++ variables-values/script.js
@@ -0,0 +1,46 @@
+// debugger;
+// let js = "amazing";
+
+/*
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.14159;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //here is a bug, will print type object
