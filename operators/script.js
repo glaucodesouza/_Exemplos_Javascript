384a4fb36030f7ee58b575d30aa82ea7e9d50167
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-07 22:52:18 -0300

    operators

---
+++ operators/script.js
@@ -0,0 +1,85 @@
+// debugger;
+// let js = "amazing";
+
+/*
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.14159;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //here is a bug, will print type object
+*/
+
+/*
+let age = 30;
+age = 31;
+
+console.log(age);
+
+const birthYear = 1991;
+// birthYear = 1990; //will generate an error: Assignment to constant variable
+console.log(birthYear);
+
+// const job; // error: Uncaught SyntaxError: Missing initializer in const declaration
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+lastName = "John"; // JAVASCRIPT will create a global varioable for this. This is wrong.
+console.log(lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//1 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "de Jesus";
+console.log(firstName, lastName);
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+
+console.log(x);
