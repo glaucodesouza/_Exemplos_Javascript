d907cb0a929b7e68fbe794bb050a341c7cb2d560
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-07-21 17:24:23 -0300

    Create messageToast.js

---
+++ messageToast.js
@@ -0,0 +1,14 @@
+sap.m.MessageToast.show("Produto " + oDados.Codigo + " criado com sucesso !", {
+                        duration: 5000,                  // default
+                        width: "15em",                   // default
+                        my: "center bottom",             // default
+                        at: "center bottom",             // default
+                        of: window,                      // default
+                        offset: "0 0",                   // default
+                        collision: "fit fit",            // default
+                        onClose: null,                   // default
+                        autoClose: true,                 // default
+                        animationTimingFunction: "ease", // default
+                        animationDuration: 1000,         // default
+                        closeOnBrowserNavigation: false   // default is true);
+                    });
