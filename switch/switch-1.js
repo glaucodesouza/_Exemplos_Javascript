f3d60d50ed011932f1709e6ccb7b6d0450e31638
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-08-19 20:44:33 -0300

    switch

---
+++ switch/switch-1.js
@@ -0,0 +1,55 @@
+/*Uee Extension Code Runner
+to test your code in VS Code terminal*/
+
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+
+/*
+Output:
+Plan course structure
+Go to coding meetup
+*/
+
+// Example of a switch statement with a IF
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
+
+/*
+Output:
+Plan course structure
+Go to coding meetup
+*/
