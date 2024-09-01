d9992370a61aa1313af2682b989e14731d22bccb
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-09-01 11:37:11 -0300

    Update split-1.js

--- split-1.js
+++ split-1.js
@@ -8,9 +8,13 @@ examples from
 let text = "How are you doing today?";
 console.log(text.split(" "));
 console.log(text.split(""));
+const myArray = text.split(" ");
 console.log(myArray[1]); //get second element from split = are
+const myArray3 = text.split(" ", 3); //split max elements in array = 3
+console.log(myArray3[2]); //get third element from split = you
 /*results:
 [ 'How', 'are', 'you', 'doing', 'today?' ]
 [ 'H', 'o', 'w', ' ', 'a', 'r', 'e', ' ', 'y', 'o', 'u', ' ', 'd', 'o', 'i', 'n', 'g', ' ', 't', 'o', 'd', 'a', 'y', '?' ]
 are
+you
 */
