1a0faeefe6d8b827944bb173632b0b757a73019e
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-09-01 11:34:12 -0300

    Create split-1.js

---
+++ split-1.js
@@ -0,0 +1,16 @@
+/*
+split a string into Array with splitted elements
+
+examples from
+  https://www.w3schools.com/jsref/jsref_split.asp
+*/
+
+let text = "How are you doing today?";
+console.log(text.split(" "));
+console.log(text.split(""));
+console.log(myArray[1]); //get second element from split = are
+/*results:
+[ 'How', 'are', 'you', 'doing', 'today?' ]
+[ 'H', 'o', 'w', ' ', 'a', 'r', 'e', ' ', 'y', 'o', 'u', ' ', 'd', 'o', 'i', 'n', 'g', ' ', 't', 'o', 'd', 'a', 'y', '?' ]
+are
+*/
