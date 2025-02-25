582a64fd25fe04bca4407a3af1003b8589ab7824
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2025-02-25 16:09:56 -0300

    date

--- date-functions-1/date-function-milliseconds-to-date.js
+++ date-functions-1/date-function-milliseconds-to-date.js
@@ -2,6 +2,14 @@ const d = new Date(853113600000).toISOString().substring(0, 19);
 console.log(d);
 //result is: 1997-01-13T00:00:00
 
-const d = new Date(1550102300000).toISOString().substring(0, 19);
+const d2 = new Date(1550102300000).toISOString().substring(0, 19);
 console.log(d);
 //result is: 1997-01-13T00:00:00
+
+
+function formatarDataApiParaArquivo(dataApi) {
+  // receives in milliseconds, dataApi=853113600000
+  // returns format: dd/mm/aaaa
+  let d = new Date(dataApi).toISOString().substring(0,19);
+  return d.substring(8, 10)+ '/' + d.substring(5, 7) + '/' + d.substring(0, 4);
+}
