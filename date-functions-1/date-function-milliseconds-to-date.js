23634e60ad5792210ce82a830bbef9465c4c089e
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2025-02-25 15:29:50 -0300

    Create date-function-milliseconds-to-date.js

---
+++ date-functions-1/date-function-milliseconds-to-date.js
@@ -0,0 +1,7 @@
+const d = new Date(853113600000).toISOString().substring(0, 19);
+console.log(d);
+//result is: 1997-01-13T00:00:00
+
+const d = new Date(1550102300000).toISOString().substring(0, 19);
+console.log(d);
+//result is: 1997-01-13T00:00:00
