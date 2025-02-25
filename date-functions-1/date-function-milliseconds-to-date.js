a9ae4972566e880f1e7c83b5406a1950b7c00e91
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2025-02-25 18:00:31 -0300

    dates in milliseconds

--- date-functions-1/date-function-milliseconds-to-date.js
+++ date-functions-1/date-function-milliseconds-to-date.js
@@ -8,8 +8,16 @@ console.log(d);
 
 
 function formatarDataApiParaArquivo(dataApi) {
-  // receives in milliseconds, dataApi=853113600000
-  // returns format: dd/mm/aaaa
+  // receives dataApi=853113600000
+  // returns dd/mm/aaaa
   let d = new Date(dataApi).toISOString().substring(0,19);
+  return d.substring(8, 10)+ '/' + d.substring(5, 7) + '/' + d.substring(0, 4);
+}
+
+
+function formatarDataApiParaArquivo(dataApi) {
+  // receives dataApi=/Date(853113600000)/
+  // returns dd/mm/aaaa
+  new Date(parseInt(dataApi.substring(6, dataApi.length - 2))).toISOString().substring(0, 19);
   return d.substring(8, 10)+ '/' + d.substring(5, 7) + '/' + d.substring(0, 4);
 }
